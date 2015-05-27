bf783f7356b0990208baccb3f79c519369e9a950
Nikita Lebedev <nikita.lebedev@ostrovok.ru>
2015-05-27 16:28:06 +0300

    insert

--- index.go
+++ index.go
@@ -3,42 +3,73 @@ package main
 
 import (
     "fmt"
+    "log"
 
     "github.com/go-martini/martini"
     "github.com/martini-contrib/render"
 
-    // "labix.org/v2/mgo"
+    "gopkg.in/mgo.v2"
+    // "gopkg.in/mgo.v2/bson"
 )
 
 
-// type PagesData struct {
-//     Number      int
-//     Description string
-//     Status      string
-// }
+type Value struct {
+    key string
+    value int
+}
 
 
-func setData() [1][3][12][5]int {
-    var data [1][3][12][5]int;
+func db_init() {
+    session, err := mgo.Dial("localhost:27017")
+    if err != nil {
+        panic(err)
+    }
+
+    defer session.Close()
+
+    c := session.DB("test").C("test")
+
+
+    const key string = "value"
+    var data [1][3][12][5]Value
 
     for i := 0; i < 3; i++ {
         for j := 0; j < 12; j++ {
             for k := 0; k < 5; k++ {
-                data[0][i][j][k] = k;
+                data[0][i][j][k] = Value{key, k}
             }
         }
     }
 
-    return data;
+    err = c.Insert(data)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
+
+// func db_find() {
+//     result := Data{}
+//     err = c.Find(bson.M{"name": "Ale"}).One(&result)
+//     if err != nil {
+//         log.Fatal(err)
+//     }
+
+//     fmt.Println("Phone:", result.Phone)
+// }
 
 
 func main() {
     app := martini.Classic()
     app.Use(render.Renderer())
 
-    app.Get("/api/v1/index", func(render render.Render, params martini.Params) {
-        render.JSON(200, setData())
+    db_init()
+
+    // app.Post("/api/v1/get", func(render render.Render) {
+    //     render.JSON(200, db_find())
+    // })
+
+    app.Post("/api/v1/set", func(render render.Render) {
+        render.JSON(200, map[string]interface{}{"success": true, "error": nil})
     })
 
     fmt.Printf("App starting!\n")
