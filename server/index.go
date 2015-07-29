e7d958e9ff21c2da471421491ccd8616e86a9623
Nikita Lebedev <nikita.lebedev@lazada.com>
2015-07-29 17:12:27 +0300

    test

--- server/index.go
+++ server/index.go
@@ -57,6 +57,7 @@ func (self *Index) db_get() string {
     db_record := []DBRecord{}
     self.MongoCollection.Find(nil).All(&db_record)
 
+
     json_result, err := json.Marshal(db_record)
     if err != nil {
         log.Fatal(err)
