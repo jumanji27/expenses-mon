e99ef94c695d1e474122a82cf35a0c50948b408a
Nikita Lebedev <nikita.lebedev@lazada.com>
2015-09-02 19:38:53 +0300

    ignpre

--- client/build/tmp/jade.js
+++
@@ -1,7 +0,0 @@
-function tmpl_layouts_p_main_index (locals) {
-var buf = [];
-var jade_mixins = {};
-var jade_interp;
-
-buf.push("<div class=\"l_main k_clear-flow js_l-main\"></div>");;return buf.join("");
-}
